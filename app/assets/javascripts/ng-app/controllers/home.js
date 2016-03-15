f893f6e444372c26ad55baf1b54e2afcf21fa974
Chirag Tailor <ctailor2@gmail.com>
2016-03-14 22:35:57 -0500

    Setup for pulling the user model into the view. Keep the form ng model stateless to prevent weird 2 way binding issues.

--- app/assets/javascripts/ng-app/controllers/home.js
+++ app/assets/javascripts/ng-app/controllers/home.js
@@ -1,16 +1,21 @@
 angular.module('AngularDoer')
   .controller('HomeCtrl', function($scope, $http) {
+    var user = {};
+
+    $scope.user = user;
+
     $http.get('http://localhost:4000/v1/todos/index').then(
       function(successResult) {
-        $scope.todos = successResult.data
+        user.todos = successResult.data;
       },
       function(errorResult) {
         // Need to handle the error
       }
     );
 
-    $scope.add = function(todo) {
-      $http.post('http://localhost:4000/v1/todos/create', { todo: todo });
+    $scope.add = function(task) {
+      //$scope.user.todos.push({ task: task });
+      //$http.post('http://localhost:4000/v1/todos/create', { todo: todo });
     };
   });
 
