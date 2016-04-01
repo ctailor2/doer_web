5097b88ddfb5ca3bdc23c6dc7a35d24e389270f6
Chirag Tailor <ctailor2@gmail.com>
2016-04-01 13:20:00 -0500

    Move todos filters to their own file

--- app/assets/javascripts/ng-app/controllers/todos.js
+++ app/assets/javascripts/ng-app/controllers/todos.js
@@ -6,39 +6,6 @@ angular.module('AngularDoer')
       }
     }
   }])
-  .filter('active', function() {
-    return function(todos, active) {
-      var filteredTodos = [];
-      angular.forEach(todos, function(todo) {
-        if(todo.active == active) {
-          filteredTodos.push(todo);
-        }
-      });
-      return filteredTodos;
-    };
-  })
-  .filter('completed', function() {
-    return function(todos, completed) {
-      var filteredTodos = [];
-      angular.forEach(todos, function(todo) {
-        if(todo.completed == completed) {
-          filteredTodos.push(todo);
-        }
-      });
-      return filteredTodos;
-    };
-  })
-  .filter('positionUpdated', function() {
-    return function(todos) {
-      var filteredTodos = [];
-      angular.forEach(todos, function(todo) {
-        if(todo.position != todos.indexOf(todo)) {
-          filteredTodos.push(todo);
-        }
-      });
-      return filteredTodos;
-    };
-  })
   .controller('TodosCtrl', function($scope, $http, $filter, UserService, TodoService, activeFilter, positionUpdatedFilter) {
     $scope.sortableOptions = {
       axis: 'y',
---
+++ app/assets/javascripts/ng-app/filters/todo.js
@@ -0,0 +1,35 @@
+angular.module('AngularDoer')
+  .filter('active', function() {
+    return function(todos, active) {
+      var filteredTodos = [];
+      angular.forEach(todos, function(todo) {
+        if(todo.active == active) {
+          filteredTodos.push(todo);
+        }
+      });
+      return filteredTodos;
+    };
+  })
+  .filter('completed', function() {
+    return function(todos, completed) {
+      var filteredTodos = [];
+      angular.forEach(todos, function(todo) {
+        if(todo.completed == completed) {
+          filteredTodos.push(todo);
+        }
+      });
+      return filteredTodos;
+    };
+  })
+  .filter('positionUpdated', function() {
+    return function(todos) {
+      var filteredTodos = [];
+      angular.forEach(todos, function(todo) {
+        if(todo.position != todos.indexOf(todo)) {
+          filteredTodos.push(todo);
+        }
+      });
+      return filteredTodos;
+    };
+  })
+
